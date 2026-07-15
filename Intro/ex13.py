8e8d8cc9cec57daa9532e6b2f11a67d1b6fcd2e9
AlexChitic20 <chiticalex2005@gmail.com>
2026-07-16 00:33:39 +0300

    Update ex13.py

--- Intro/ex13.py
+++ Intro/ex13.py
@@ -1,17 +1,16 @@
 def is_palindrome(number):
-    """Checks whether a number reads the same forwards and backwards."""
+    
     text = str(number)
     return text == text[::-1]
 
 
 def has_even_number_of_digits(number):
-    """Checks whether a number has an even number of digits."""
+    
     return len(str(number)) % 2 == 0
 
 
 def palindromes_with_even_digits(numbers):
-    """Returns a new list containing only the palindromes from `numbers`
-    that have an even number of digits."""
+    
     result = []
     for number in numbers:
         if is_palindrome(number) and has_even_number_of_digits(number):
