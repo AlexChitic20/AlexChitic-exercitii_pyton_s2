8f3d7d0b167113951e9318151b011e76afe5cec9
AlexChitic20 <chiticalex2005@gmail.com>
2026-07-16 00:56:22 +0300

    Update ex9.py

--- Set2/ex9.py
+++ Set2/ex9.py
@@ -1,6 +1,5 @@
 def sort_by_third_char(pairs):
-    """Sorts a list of tuples of strings by the 3rd character of the
-    2nd element of each tuple."""
+  
     return sorted(pairs, key=lambda pair: pair[1][2])
 
 
