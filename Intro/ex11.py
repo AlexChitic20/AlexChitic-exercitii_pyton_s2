6c552112d59631b577bce9762820f2a00e220666
AlexChitic20 <chiticalex2005@gmail.com>
2026-07-16 00:28:49 +0300

    Add ex11

---
+++ Intro/ex11.py
@@ -0,0 +1,15 @@
+def mean_and_geomean_of_extremes(numbers):
+    """Returns the mean and geometric mean of the largest and smallest
+    elements of the list."""
+    smallest = min(numbers)
+    largest = max(numbers)
+
+    mean = (smallest + largest) / 2
+    geometric_mean = (smallest * largest) ** 0.5
+
+    return mean, geometric_mean
+
+
+if __name__ == "__main__":
+    mean, geomean = mean_and_geomean_of_extremes([4, 1, 9, 3])
+    print(f"mean={mean}, geometric mean={geomean}")
