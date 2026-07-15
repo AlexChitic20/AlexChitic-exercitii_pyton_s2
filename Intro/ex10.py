3ff78966bf5602e8c3ac11b9d33a00c0a96c7e87
AlexChitic20 <chiticalex2005@gmail.com>
2026-07-16 00:28:49 +0300

    Add ex10

---
+++ Intro/ex10.py
@@ -0,0 +1,8 @@
+def print_elements(items):
+    """Prints every element of a list using a for loop."""
+    for item in items:
+        print(item)
+
+
+if __name__ == "__main__":
+    print_elements([1, 2, 3, "hello", 4.5])
