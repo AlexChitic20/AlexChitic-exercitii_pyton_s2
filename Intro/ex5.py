a4988dbcae7f44a9dcafedbd882d6e40b38973bb
AlexChitic20 <chiticalex2005@gmail.com>
2026-07-16 00:28:48 +0300

    Add ex5

---
+++ Intro/ex5.py
@@ -0,0 +1,9 @@
+def count_digits_of_power(base, exponent):
+    """Returns how many digits `base ** exponent` has."""
+    number = base ** exponent
+    return len(str(number))
+
+
+if __name__ == "__main__":
+    digits = count_digits_of_power(2, 1024)
+    print(f"2^1024 has {digits} digits")
